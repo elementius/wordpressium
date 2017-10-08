d4c07acbe583c4846535bdfa11c7ad962271b8a9
Jamieson Roberts <hello@jamiesonroberts.ca>
2017-10-08 13:48:34 -0400

    Moving elements to a bootstrap folder

--- config/app.php
+++ config/app.php
@@ -4,15 +4,6 @@
  * accessible directory. Also define that Wordpress should display the
  * rendered theme files instead of just simply parsing the URL.
  */
-define('ROOTPATH', dirname(__DIR__));
 define('WEBROOT', ROOTPATH . '/public');
 define('WP_USE_THEMES', true);
 
-
-/**
- * Throw exception if you are missing your .env file
- */
-if (!file_exists(ROOTPATH . '/.env')) {
-    echo 'You are missing an environment file.';
-    die;
-}
