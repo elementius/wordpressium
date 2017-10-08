d0452b5ab6504afbf2e61036eb87ca934b90b2a5
Jamieson Roberts <hello@jamiesonroberts.ca>
2017-10-08 14:04:13 -0400

    CHanging things to be a composer autoload

--- public/wp-config.php
+++ public/wp-config.php
@@ -6,6 +6,5 @@
  */
 
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
-require_once(dirname(__DIR__) . '/bootstrap/app.php');
 require_once(dirname(__DIR__) . '/config/app.php');
 require_once(ABSPATH . 'wp-settings.php');
