00f183045c198d1de06e8ea8e042bd8be9c17f75
Jamieson Roberts <hello@jamiesonroberts.ca>
2017-10-08 13:49:35 -0400

    Adjusting loading of files for now

--- public/wp-config.php
+++ public/wp-config.php
@@ -6,5 +6,6 @@
  */
 
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
+require_once(dirname(__DIR__) . '/bootstrap/config.php');
 require_once(dirname(__DIR__) . '/config/app.php');
 require_once(ABSPATH . 'wp-settings.php');
