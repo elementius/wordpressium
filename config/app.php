ad6514420f56055cef4c205d0465d2ae881bf13b
Jamieson Roberts <hello@jamiesonroberts.ca>
2017-10-09 13:24:01 -0400

    Debugging and environment specific settings

--- config/app.php
+++ config/app.php
@@ -17,7 +17,7 @@ define('WPENV', env('WPENV', 'production'));
  * This allows for easier moving between local, staging, and production
  * environments without having to worry about adjusting the database.
  */
-define('WP_HOME', hostURL() );
+define('WP_HOME', hostURL());
 define('WP_SITEURL', hostURL() . '/app');
 
 /**
@@ -89,7 +89,7 @@ define('WP_AUTO_UPDATE_CORE', 'minor');
 /**
  * One of the primary things this Wordpress aims to solve is the file structure
  * mimicking some of Laravel's setup. This moves around Wordpress' folders to a
- * more sane structure while leaving the core files in a subfolder the way it
+ * more sane structure while leaving the core files in a sub-folder the way it
  * should be.
  */
 define('WP_CONTENT_DIR', WEBROOT);
@@ -97,3 +97,47 @@ define('WP_CONTENT_URL', WP_HOME);
 define('WPMU_PLUGIN_DIR', WEBROOT . '/plugins/required');
 define('WPMU_PLUGIN_URL', WP_HOME . '/plugins');
 define('UPLOADS', '../uploads');
+
+/**
+ * Wordpress insists on outputting its logging to the public folder which is a
+ * security hole in and of itself. This enables the same level of logging, while
+ * also disabling Wordpress logging. It also sets up debug display based on the
+ * previously defined environment.
+ */
+define('WP_DEBUG', true);
+define('WP_DEBUG_LOG', false);
+ini_set('log_errors', 1);
+ini_set('error_log', dirname(__DIR__) . '/log/wordpress.log');
+switch (WPENV) {
+    case 'local':
+    case 'development':
+    case 'dev':
+        define('WP_DEBUG_DISPLAY', true);
+        break;
+    default:
+        define('WP_DEBUG_DISPLAY', false);
+        break;
+}
+
+/**
+ * Environment specific non debug settings. There are several things that while
+ * are fine in your working environments, should probably be locked down when
+ * you are in production, for safety, speed, and user experience. This uses the
+ * constant WPENV which is set above and that will default to production when
+ * nothing is set. Defaults are not reset, and only things that are modifications
+ * are set here.
+ *
+ * NOTE: DISALLOW_FILE_MODS Is set for production to avoid end users installing
+ * anything that could modify the environment and break a production environment.
+ */
+switch (WPENV) {
+    case 'local':
+    case 'development':
+    case 'dev':
+        define('SAVEQUERIES', true);
+        define('SCRIPT_DEBUG', true);
+        break;
+    default:
+        define('DISALLOW_FILE_MODS', true);
+        break;
+}
